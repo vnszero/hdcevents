5b71892b4cd1796ce831114c8324322cb6f0e3d7
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-04-20 15:06:23 -0300

    upload images

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,6 +30,22 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+            //recover from form hash
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+
+            // set a unique name based in file name and time
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+            
+            // store in a exclusive path just for this images
+            $request->image->move(public_path('img/events'), $imageName);
+
+            // prepare to save a link in database
+            $event->image = $imageName;
+        }
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento Criado com Sucesso!');
