89cad3ad1d4b3b77be27eb3cdffc0ff5f1c3b14e
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-04-20 12:56:12 -0300

    Flash Message

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,7 +32,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento Criado com Sucesso!');
 
     }
 
