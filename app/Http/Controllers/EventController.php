ad5f073b23b111e6f3e8fcb6bc8e6da48884db60
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-04-22 13:44:21 -0300

    events details

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -52,4 +52,8 @@ class EventController extends Controller
 
     }
 
+    public function show($id) {
+        $event = Event::findOrFail($id);
+        return view('events.show', ['event' => $event]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
 Route::post('/events', [EventController::class, 'store']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 
 Route::get('/contact', function () {
     return view('contact');
