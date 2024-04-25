19cdbe304ed521f152687b30c5a53a0baa8c6327
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-04-25 12:00:52 -0300

    Delete functionality

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
+// get
 Route::get('/', [EventController::class, 'index']);
-Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+// post
+Route::post('/events', [EventController::class, 'store']);
+
+// delete
+Route::delete('events/{id}', [EventController::class, 'destroy']);
 
 Route::get('/contact', function () {
     return view('contact');
